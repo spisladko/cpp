54b8b0d95376945ed8b309d92f457bc2aadaab85
spisladko <inessaivanovastr@inbox.ru>
2020-03-29 19:40:12 +0300

    я сделяль

---
+++ lab_01/1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+double func(double n) {
+    //double value = -n*log(n);
+	//double value = sqrt(n);
+	double value = -(pow(n,2))-0.5;
+    return value;
+}
+
+vector<vector<char> > get_grid(double (*func)(double), double a, double b, int width, int height) {
+    vector<double> values;
+	vector<vector<char> > grid(height, vector<char>(width, ' '));
+	vector<char> axes (width, '-');
+    double n, step, value, min, max;
+	int i, j, zero;
+    n = a;
+	min = 0;
+	max = 0;
+	
+    step = (b-a)/width;
+	
+	for (int i = 0; i < width; i++) {
+        value = func(n);
+        values.push_back(value);
+        n += step;
+	}
+
+	for (double value: values) {
+		if (value < min) { min = value; }
+		if (value > max) { max = value; }
+	}
+	
+	double k = height/(abs(max)+abs(min));
+	
+		
+    if (min == 0) { zero = height; }
+	else if (max == 0) { zero = -1; }
+	else { zero = abs(round(max * k)); }
+	
+	cout << zero << endl;
+	
+	for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+			if (i == zero)  { grid[i][j] = '-'; }
+		}
+	}
+	
+	for (i = 0; i < values.size(); i++) {
+			values[i] = round(values[i]*k);
+	}
+	
+	for (int j = 0; j < values.size(); j++) {
+	
+		if (values[j] > 0) {
+			for (int i = (zero - 1); i >= (zero - values[j]); i--) {
+				grid[i][j] = '#';
+			}
+			
+		} else if (values[j] < 0) {
+			for (int i = (zero + 1); i < (zero + abs(values[j])); i++) {
+				grid[i][j] = '#';
+			} 
+		} else {}	
+	}
+	
+	
+	if (min == 0) { grid.push_back(axes); }
+	if (max == 0) { grid.insert(grid.begin(),axes); }
+	
+	for (vector<char> line: grid) {
+		for (char element: line) {
+			cout << element;
+		}
+		cout << endl;
+	}
+	
+	return grid;
+}
+
+int draw_grid(vector<vector<char> > grid) {
+	ofstream fout;
+	fout.open("file3.txt");
+	
+	for (vector<char> line: grid) {
+		for (char element: line) {
+			fout << element;
+		}
+		fout << endl;
+	}
+	return 0;
+}
+ 
+int main() {
+    int width, height, i, j;
+    double a,b;
+	vector<vector<char> > table;
+    cout << "Количество столбиков и высота графика" << endl;
+    cin >> width >> height;
+    cout << "Начало и конец отрезка" << endl;
+    cin >> a >> b;
+
+	draw_grid(get_grid(func, a, b, width, height));
+
+    return 0;
+}
+
