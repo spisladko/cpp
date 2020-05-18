f28dc8fe306a648a49b39d34f20d3e9ca20ad55c
spisladko <inessaivanovastr@inbox.ru>
2020-05-18 17:07:44 +0300

    bugs fixed

--- lab_02.cpp
+++ lab_02.cpp
@@ -11,7 +11,6 @@ public:
 
     safe_long_long_t operator=(const safe_long_long_t &a) {
         value = a.value;
-<<<<<<< HEAD
         return *this;
     }
 
@@ -40,36 +39,6 @@ public:
         return *this;
     }
 
-=======
-		return *this;
-    }
-
-	safe_long_long_t &operator+=(const safe_long_long_t& a) {
-		value += a.value;
-		return *this;
-	}
-
-	safe_long_long_t &operator-=(const safe_long_long_t& a) {
-		value -= a.value;
-		return *this;
-	}
-	
-	safe_long_long_t &operator*=(const safe_long_long_t& a) {
-		value *= a.value;
-		return *this;
-	}
-	
-	safe_long_long_t &operator/=(const safe_long_long_t& a) {
-		value /= a.value;
-		return *this;
-	}
-	
-	safe_long_long_t &operator%=(const safe_long_long_t& a) {
-		value %= a.value;
-		return *this;
-	}
-	
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
 //префиксный инкремент
     safe_long_long_t& operator++() {
         value++;
@@ -77,33 +46,19 @@ public:
     }
 //постфиксный инкремент
     safe_long_long_t operator++(int i) {
-<<<<<<< HEAD
         safe_long_long_t oldValue = *this;
         ++*this;
-=======
-        safe_long_long_t oldValue(value);
-        value++;
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
         return oldValue;
     }
 //префиксный декремент
     safe_long_long_t& operator--() {
-<<<<<<< HEAD
         --value;
-=======
-        value--;
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
         return *this;
     }
 //постфиксный декремент
     safe_long_long_t operator--(int i) {
-<<<<<<< HEAD
         safe_long_long_t oldValue = *this;
         --*this;
-=======
-        safe_long_long_t oldValue(value);
-        value--;
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
         return oldValue;
     }
 
@@ -116,21 +71,12 @@ public:
         in >> a.value;
         return in;
     };
-<<<<<<< HEAD
 
     friend bool operator==(const safe_long_long_t& a, const safe_long_long_t& b);
     friend bool operator>(const safe_long_long_t& a, const safe_long_long_t& b);
     friend bool operator<(const safe_long_long_t& a, const safe_long_long_t& b);
     friend bool operator>=(const safe_long_long_t& a, const safe_long_long_t& b);
     friend bool operator<=(const safe_long_long_t& a, const safe_long_long_t& b);
-=======
-	
-	friend bool operator==(const safe_long_long_t& a, const safe_long_long_t& b); 
-	friend bool operator>(const safe_long_long_t& a, const safe_long_long_t& b);
-	friend bool operator<(const safe_long_long_t& a, const safe_long_long_t& b);
-	friend bool operator>=(const safe_long_long_t& a, const safe_long_long_t& b);
-	friend bool operator<=(const safe_long_long_t& a, const safe_long_long_t& b);
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
 };
 
 safe_long_long_t operator+(safe_long_long_t a, const safe_long_long_t& b) {
@@ -187,7 +133,6 @@ public:
         size = n;
         arr = new int[size];
     }
-<<<<<<< HEAD
 
     safe_c_array_t &operator=(const safe_c_array_t& arr2) {
         if (this != &arr2) {
@@ -201,40 +146,20 @@ public:
         return *this;
     }
 
-=======
-	
-	safe_c_array_t &operator=(const safe_c_array_t& arr2) {
-		for (int i = 0; i < size; i++) {
-			arr[i] = arr2.arr[i];
-		}
-	}
-			
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
     safe_c_array_t (const safe_c_array_t& origin) {
         size = origin.size;
         arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = origin.arr[i];
-<<<<<<< HEAD
         }
     }
 
-=======
-		}
-		return *this;
-    }
-	
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
     void fill() {
         for (int i = 0; i < size; i++) {
             arr[i] = i;
         }
     }
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2cb76aa16d70ddd015ab59dc5e4a3d920c94a5cd
     int &operator[](int k) {
         return arr[k];
     }
